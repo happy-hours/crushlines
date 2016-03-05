8622779d8689925d68d08802ea53d0847017b55c
Mina <wilhelmina.jane@gmail.com>
2016-03-05 14:29:45 +0000

    fixed carousel, hidden arrow using display none

--- views/components/slider.jsx
+++ views/components/slider.jsx
@@ -1,24 +1,24 @@
 'use strict'
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Slider = require('react-slick');
 
 var LandingSlider = React.createClass({
+    componentDidMount () {
+        $('.slider-custom').slick({
+            infinite:true,
+            dots: true
+        });
+    },
     render: function () {
-        var settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
 
         return (
-            <Slider className='slider-custom' {...settings}>
-                <div className="slider1 col s12"><h3>1</h3></div>
-                <div className="slider2 col s12"><h3>2</h3></div>
-                <div className="slider3 col s12"><h3>3</h3></div>
-            </Slider>
+            <div className='slider-custom'>
+                <div className="slider1"><h3>1</h3></div>
+                <div className="slider2"><h3>2</h3></div>
+                <div className="slider3"><h3>3</h3></div>
+            </div>
         );
     }
 });
